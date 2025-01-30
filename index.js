f5475bdb12ff4173f591c12b7b80ff1f5f60342e
davidwp009 <142822982+davidwp009@users.noreply.github.com>
2025-01-30 16:59:47 +0200

    gg

---
+++ index.js
@@ -0,0 +1,13 @@
+//const new = require('./test');
+
+//console.log(new.hello());
+//cohsole,log(new.bye());
+
+//import { square, diag } from "./test";
+//import * as myModul from '.test';
+//console.log(square(11));
+//console.log(diag(4, 3));
+
+import one from '.test';
+one();
+
---
+++ test.js
@@ -0,0 +1,21 @@
+//const helloM = 'hello!';
+//const byeM = 'goodbye';
+
+//const hello = () => helloM;
+//const bye = () => byeM;
+
+//module.exports = {
+//    hello,
+//    bye,
+
+//}
+
+
+
+//const sqrt = maath.sqtr;
+//const square = x => x * x;
+//const diag = (x, y) => sqrt(square(x) + square(y));
+
+//export {square, diag};
+
+export default function one(){...}
